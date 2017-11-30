e5a1c3abd017aa49f3f1a21a691efe3d4ae32cf6
rahul <rahulsingh2240@gmail.com>
2017-11-30 15:07:01 +0530

    bootstrap add edit routes

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use(bodyParser.urlencoded({ extender: false }))
 
 app.use(bodyParser.json())
 
+// set public folder
+app.use(express.static(path.join(__dirname,'public')));
+
 // Routes
 
 app.get('/', function(req, res){
@@ -71,6 +74,49 @@ app.post('/articles/add', function(req, res){
   });
   return;
 });
+
+app.get('/article/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('article', {
+      title: article.title,
+      article: article
+    });
+    return;
+  });
+});
+
+// Edit article
+app.get('/article/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article', {
+      title: 'Edit:' + article.title,
+      article: article
+    });
+    return;
+  });
+});
+
+app.post('/article/edit/:id', function(req, res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article,function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.redirect('/');
+    }
+  });
+  return;
+});
+
+
+
 
 // Start a node server
 app.listen(3000, function(){
