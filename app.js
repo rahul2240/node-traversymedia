460b28ab028e188d87822c44ad0d749e8f768887
rahul <rahulsingh2240@gmail.com>
2017-12-01 02:00:28 +0530

    update and delete

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
 mongoose.connect('mongodb://localhost/restapi');
 
 let db = mongoose.connection;
@@ -27,8 +32,26 @@ app.use(bodyParser.urlencoded({ extender: false }))
 
 app.use(bodyParser.json())
 
+// Use validator
+app.use(expressValidator());
+
 // set public folder
 app.use(express.static(path.join(__dirname,'public')));
+
+//Express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+//Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 
 // Routes
 
@@ -57,22 +80,38 @@ app.get('/articles/add', function(req, res){
   });
 });
 
-
 app.post('/articles/add', function(req, res){
-  let article = new Article();
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
 
-  article.save(function(err){
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.redirect('/');
-    }
-  });
-  return;
+  req.checkBody('title','Title is required').notEmpty();
+  req.checkBody('author','Author is required').notEmpty();
+  req.checkBody('body','Body is required').notEmpty();
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('add_article', {
+      errors: errors,
+      title: 'New article'
+    });
+  }else{
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err){
+      if(err){
+        console.log(err);
+        return;
+      }
+      else{
+        req.flash('success','Article Added');
+        res.redirect('/');
+      }
+    });
+  }
+
+
 });
 
 app.get('/article/:id', function(req, res){
@@ -109,14 +148,21 @@ app.post('/article/edit/:id', function(req, res){
       console.log(err);
     }
     else{
+      req.flash('success','Article updated');
       res.redirect('/');
     }
   });
   return;
 });
 
-
-
+// Delete article
+app.delete('/article/:id', function (req, res) {
+  let query = {_id:req.params.id}
+    Article.remove(query, function(err) {
+        if (err) return res.status(500).send("There was a problem deleting the article.");
+        res.status(200).send("Article was deleted.");
+    });
+});
 
 // Start a node server
 app.listen(3000, function(){
